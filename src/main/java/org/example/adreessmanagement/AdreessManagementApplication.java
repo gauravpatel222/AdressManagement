21b3e24fa42b3922358c36cd95e6ff4299d40863
Gaurav <gauravgurjar8791@gmail.com>
2025-03-06 16:01:45 +0530

    Initialized

---
+++ src/main/java/org/example/adreessmanagement/AdreessManagementApplication.java
@@ -0,0 +1,13 @@
+package org.example.adreessmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdreessManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdreessManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/adreessmanagement/AdreessManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.adreessmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdreessManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
